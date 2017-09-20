4a360df2a6d73e71b4db83613e220ef95b451fdc
gion <bogdan.gradinariu@thinslices.com>
2017-09-21 01:44:23 +0300

    Added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NothingComponent } from './nothing';
+import { LocationListComponent } from './location';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/nothing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'nothing',
+    component: NothingComponent
+  },
+  {
+    path: 'locations',
+    component: LocationListComponent,
+    data: { title: 'Select a location!' }
+  },
+  // {
+  //   path: 'locations/:id',
+  //   component: AppComponent,
+  //   data: { title: 'Select a location!' }
+  // },
+  // {
+  //   path: 'locations',
+  //   component: AppComponent,
+  //   data: { title: 'Select a location!' }
+  // },
+  // { path: '**', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NothingComponent,
+    LocationListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
