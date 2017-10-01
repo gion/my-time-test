7e7646f151360b48f767781f8ac88b52763e181a
gion <bogdan.gradinariu@thinslices.com>
2017-10-01 18:09:02 +0300

    unit tests for main component

--- src/app/nothing/nothing.component.spec.ts
+++ src/app/nothing/nothing.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from "@angular/router/testing";
+
 import { NothingComponent } from './nothing.component';
 
 describe('NothingComponent', () => {
@@ -8,6 +10,7 @@ describe('NothingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ NothingComponent ]
     })
     .compileComponents();
@@ -22,4 +25,10 @@ describe('NothingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a link to `/locations`', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    const href = compiled.querySelector('a').getAttribute('href');
+    expect(href).toEqual('/locations');
+  }));
 });
