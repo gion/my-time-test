d408df881ec71c733c9ecfc138289034163c4294
gion <bogdan.gradinariu@thinslices.com>
2017-10-02 17:10:55 +0300

    first e2e journey

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,30 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  private _navigateTo(path:string = '') {
+    return browser.get(`${browser.baseUrl}${path}`);
+  }
+
+  navigateTo(path:string = '') {
+    return this
+      ._navigateTo(path)
+      .then(() => browser.waitForAngular());
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getBookingButton() {
+    return element(by.css('app-root a[md-button]'));
+  }
+
+  getSpinner() {
+    return element(by.css('app-root md-spinner'));
+  }
+
+  getUrl() {
+    return browser.getCurrentUrl()
+      .then(url => url.replace(browser.baseUrl, ''));
   }
 }
